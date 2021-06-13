144685399b13843fcbec0e4886833d95b12b01ba
OGRemix <natehanks05@gmail.com>
2021-06-13 03:53:17 +0000

    add platforms and collectables to game

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -37,8 +37,11 @@
         createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
 
         // example:
-        createPlatform(400, 250);
-        createPlatform(170, 460, 0.5);
+        createPlatform(400, 430);
+        createPlatform(170, 550, 1,);
+        createPlatform(120, 318)
+        createPlatform(400, 200);
+        
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
